c715d3057d07018242642750ea64c3b18b3bf2b3
Jagannadham-Poojitha <112007928+Jagannadham-Poojitha@users.noreply.github.com>
2026-03-04 12:40:51 +0530

    Delete Day-6 directory

--- Day-6/continue.py
+++
@@ -1,5 +0,0 @@
-#continue
-for i in range(1,10):
-    if i==7:
-        continue
-    print(i)
--- Day-6/for-loop-set.py
+++
@@ -1,3 +0,0 @@
-followers={'Pooja','keerthi','teju','divya','hima'}
-for i in followers:
-    print(f'{i}-|follow back|remove|message|')
